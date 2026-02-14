bef13640bf580fe8a449d78301ff21255079b156
dabojimenez <dabojimenez16@hotmail.com>
2026-02-13 22:36:17 -0500

    variables

--- ProgramacionEstructurada/Program.cs
+++ ProgramacionEstructurada/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int number = 15;
+number = 16;
+
+// presision de 7 digitos
+float decimalvar = 15.6f;
+// Necesidad mametamica bien grade usar un double
+// tiene una presision de 15 digitos
+double dou = 15.8;
+
+// true o false
+bool existe = false;
+
+// cadenas alfanumericas
+string cadena = "aasewe";
+char unaletra = '3';
+
+// var: infiere su tipo con lo que tiene a la derecha, y se quedara con ese tipo de dato
+var ee = 58;
+var nombre = "sdasd";
